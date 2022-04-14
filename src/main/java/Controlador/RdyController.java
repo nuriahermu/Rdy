c953a54849cff3ce7dead97b072e5d4babb30dde
Nuria <Nuria@DESKTOP-8L6BNK5.home>
2022-04-15 01:07:06 +0200

    Compilacion correcta

--- src/main/java/Controlador/RdyController.java
+++ src/main/java/Controlador/RdyController.java
@@ -4,7 +4,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
 import Modelo.Blog;
 import Modelo.ComentarioBlog;
 import Modelo.ComentarioJuego;
@@ -15,13 +24,7 @@ import ModeloBBDD.ComentariosDAO;
 import ModeloBBDD.JuegosDAO;
 import ModeloBBDD.NoticiasDAO;
 import ModeloBBDD.UsuariosDAO;
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.MultipartConfig;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.Part;
+
 
 @MultipartConfig
 @WebServlet(name = "rdyController", urlPatterns = {"/rdyController"})
